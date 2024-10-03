272af40172b7d953068c13627eba09f9e1ed24c2
raiden <nadirkichou@hotmail.fr>
2024-10-03 11:05:29 +0200

    feat: use the load function

--- src/main/index.py
+++ src/main/index.py
@@ -1,5 +1,13 @@
+from image_loader.loader import load
+
+TESTING_IMAGE_PATH = "data/testing-image.jpg"
+
 def main():
     print("[image-enhancer]: welcome to the program!")
+    
+    image = load(TESTING_IMAGE_PATH)
+    
+    image.show()
     
 if __name__ == "__main__":
     main()
