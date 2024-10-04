298c519960fd5b655ca1de42e477396c8a9fb34f
raiden <nadirkichou@hotmail.fr>
2024-10-04 22:58:12 +0200

    fix: fixed the function

--- src/pixels_processors/average_pixels_processor.py
+++ src/pixels_processors/average_pixels_processor.py
@@ -5,4 +5,7 @@ from pixels_processors.generic_pixels_processor import GenericPixelsProcessor
 
 class AveragePixelsProcessor(GenericPixelsProcessor):
     def getRGBColorFromPixels(self: 'AveragePixelsProcessor', pixels: np.ndarray) -> Tuple[int, int, int]:
-        pass
+        avg_color = np.mean(pixels, axis=0)
+        avg_color = tuple(avg_color.astype(int))
+        
+        return avg_color
