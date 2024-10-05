7b0645755579637c0b688aef1c65d401992d26c7
raiden <nadirkichou@hotmail.fr>
2024-10-05 09:35:37 +0200

    fix: correct typing error

--- src/image_processors/square_grid_image_processor.py
+++ src/image_processors/square_grid_image_processor.py
@@ -6,16 +6,16 @@ class SquareGridImageProcessor(GenericGridImageProcessor):
     def __init__(self, square_size: int):
         self.square_size = square_size
     
-    def convertToGridCoordinatesFromPixelCoordinates(self: 'GenericGridImageProcessor', x: int, y: int) -> Tuple[int, ...]:
+    def convertToGridCoordinatesFromPixelCoordinates(self: 'SquareGridImageProcessor', x: int, y: int) -> Tuple[int, ...]:
         square_x_coordinate = (x // self.square_size)
         square_y_coordinate = (y // self.square_size)
         
         return (square_x_coordinate, square_y_coordinate)
     
-    def getCoordinatesStartingPosition(self: 'GenericGridImageProcessor', grid_x: int, grid_y: int) -> Tuple[int, ...]:
+    def getCoordinatesStartingPosition(self: 'SquareGridImageProcessor', grid_x: int, grid_y: int) -> Tuple[int, ...]:
         return (grid_x * self.square_size, grid_y * self.square_size)
 
-    def drawGridElement(self: 'GenericGridImageProcessor', context: Context, pos_x: int, pos_y: int, color: Tuple[int, int, int]) -> None:
+    def drawGridElement(self: 'SquareGridImageProcessor', context: Context, pos_x: int, pos_y: int, color: Tuple[int, int, int]) -> None:
         context.rectangle(pos_x, pos_y, self.square_size, self.square_size)
         context.set_source_rgb(color[0], color[1], color[2])
         context.fill_preserve()
