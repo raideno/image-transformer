8bafff5739a8422d43f080853d118debaa581904
raiden <nadirkichou@hotmail.fr>
2024-10-17 17:04:42 +0200

    fix: make configurations file path absolute

--- src/image_transformer/utils/configurations_loader.py
+++ src/image_transformer/utils/configurations_loader.py
@@ -3,6 +3,7 @@ This module provides functionality to load configurations from a TOML file.
 """
 
 import tomllib
+import os
 
 def load_configurations(configuration_file_path: str) -> dict:
     """
@@ -16,7 +17,11 @@ def load_configurations(configuration_file_path: str) -> dict:
     """
     configurations = None
     
-    with open(configuration_file_path, "rb") as file:
+    project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+  
+    absolute_configuration_file_path = os.path.join(project_root, configuration_file_path)
+    
+    with open(absolute_configuration_file_path, "rb") as file:
         configurations = tomllib.load(file)
         
     return configurations
